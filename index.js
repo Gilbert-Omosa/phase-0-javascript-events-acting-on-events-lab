8922ee1eb3720344c8e671d75ba495e03e09241e
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-17 17:19:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10) - 10;
+    
+    if (left >= 0) {
+      dodger.style.left = `${left}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace('px', '');
+    const right = parseInt(rightNumbers, 10) + 10;
+    
+    if (right <= 360) {
+      dodger.style.left = `${right}px`;
+    }
+  }
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
+
